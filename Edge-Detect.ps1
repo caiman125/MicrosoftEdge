a57aba235f6b0734a3e68f789a19699f65665b56
Mohamed Bahy <64994275+caiman125@users.noreply.github.com>
2026-08-11 17:50:15 +0200

    Add PowerShell script to check Edge installation

---
+++ Edge-Detect.ps1
@@ -0,0 +1,35 @@
+Function Get-RdpSessions
+{
+$processinfo = Get-WmiObject -Query "select * from win32_process where name='explorer.exe'"
+
+$processinfo | ForEach-Object { $_.GetOwner().User } | Sort-Object -Unique | ForEach-Object { New-
+Object psobject -Property @{Computer=$computername;LoggedOn=$_} }
+
+}
+$session = Get-RdpSessions | select -expand LoggedOn
+$drive = $Env:HOMEDRIVE
+$existe = 0
+if(Test-Path -path ("C:\Program Files\Microsoft\Edge"))
+{
+$existe = 1
+Write-Host "existe"
+}
+$existe2 = 0
+if (Test-Path -path ($drive + "\Users\" + $session + "\AppData\Local\Microsoft\Edge"))
+{
+$existe2 = 1
+Write-Host "existe2"
+}
+$existe3 = 0
+if(Test-Path -path ("C:\Program Files (x86)\Microsoft\Edge\"))
+{
+$existe3 = 1
+Write-Host "existe3"
+}
+if(($existe -eq 1) -or ( $existe2 -eq 1) -or ($existe3 -eq 1))
+{
+Write-Host "Edge is installed"
+}
+else{
+Write-Host "Edge is not installed"
+}
